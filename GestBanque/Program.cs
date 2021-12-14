8ea6342f0405c7f447fe135a937fc90eb84ce1f4
TMCognitic <thierry.morre@cognitic.be>
2021-12-14 16:02:36 +0100

    Ajoutez des fichiers projet.

---
+++ GestBanque/Program.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+
+namespace GestBanque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Personne thierry = new Personne()
+            {
+                Nom = "Morre",
+                Prenom = "Thierry",
+                DateNaiss = new DateTime(1974, 06, 05)
+            };
+
+            Courant courant = new Courant()
+            {
+                Numero = "00001",
+                Titulaire = thierry,
+                LigneDeCredit = 500
+            };
+
+            courant.Depot(500);
+            courant.Depot(-500);
+            courant.Retrait(700);
+            courant.Retrait(700);
+            courant.Retrait(-700);
+
+            Console.WriteLine(courant.Solde);
+        }
+    }
+}
---
+++ Models/Courant.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Models
+{
+    public class Courant
+    {
+        private string _numero;
+        private double _solde, _ligneDeCredit;
+        private Personne _titulaire;
+
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public double Solde
+        {
+            get
+            {
+                return _solde;
+            }
+
+            private set
+            {
+                _solde = value;
+            }
+        }
+
+        public double LigneDeCredit
+        {
+            get
+            {
+                return _ligneDeCredit;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("La valeur de la ligne de crédit doit être positive");
+                    return; //Erreur
+                }
+
+                _ligneDeCredit = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get
+            {
+                return _titulaire;
+            }
+
+            set
+            {
+                _titulaire = value;
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            if (montant <= 0)
+            {
+                Console.WriteLine("Montant invalide");
+                return; //Erreur
+            }
+
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            if (montant <= 0)
+            {
+                Console.WriteLine("Montant invalide");
+                return; //Erreur
+            }
+
+            if (Solde - montant < -LigneDeCredit)
+            {
+                Console.WriteLine("Solde Insuffisant");
+                return; //Erreur
+            }
+
+            Solde -= montant;
+        }
+    }
+}
